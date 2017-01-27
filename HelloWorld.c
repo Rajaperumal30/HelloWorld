7c40fedff0646b2827cb97f9693a1e9a91fe63ea
Sundar <sundar0443@live.com>
2017-01-28 05:27:09 +0530

    HelloWorld

---
+++ HelloWorld.c
@@ -0,0 +1,23 @@
+/*File @Helloworld.c
+*bref:Simple Kernel Module to load and unload 
+*/
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int hello_init(void)
+{
+  printk(KERN_DEBUG "Hello World!\n");
+  return 0;
+}
+
+static void hello_exit(void)
+{
+  printk(KERN_DEBUG "Module Unloaded.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
